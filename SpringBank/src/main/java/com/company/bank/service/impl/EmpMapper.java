3fdd2697730cf2a82c4b1da2fe71bcc07352d0cb
Park <qseft523125@naver.com>
2021-03-15 12:56:18 +0900

    List<Map<String,Object>> 설명 추가

--- SpringBank/src/main/java/com/company/bank/service/impl/EmpMapper.java
+++ SpringBank/src/main/java/com/company/bank/service/impl/EmpMapper.java
@@ -4,5 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface EmpMapper {
+	// result값이 List<Map> 형태라는 의미
+	// Map은 String,Object 형태의 K,V를 가짐
 	public List<Map<String, Object>> getEmpList();
 }
