9485aa7b7361f20e1a0267b2a994ccd0216aa07a
parkq5231 <parkq5231>
2021-03-16 21:46:08 +0900

    과제 2번 데이터 형변환(List->json)

--- SpringBank/src/main/java/com/company/common/JsonUtil.java
+++ SpringBank/src/main/java/com/company/common/JsonUtil.java
@@ -2,13 +2,16 @@ package com.company.common;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class JsonUtil {
 
-	//
+	// Map -> json
 	public String toJson(Map<String, Object> map) {
 		StringBuilder result = new StringBuilder();
 		result.append("{");//
@@ -19,7 +22,7 @@ public class JsonUtil {
 			String key = keys.next();
 			String value = (String) map.get(key);
 			if (!start) {
-				result.append(", ");
+				result.append(",");
 			} else {
 				start = false;
 			}
@@ -32,41 +35,66 @@ public class JsonUtil {
 		return result.toString();
 	}
 
-	//
-	public String toJson(List<Map<String, Object>> map) {
+	// List -> json
+	public String toJson(List<Map<String, Object>> list) {
 		StringBuilder result = new StringBuilder();
-		result.append("[");//
+		result.append("{");//
 		// to do
 		// {},{}
-		map.size();
-
-		result.append("]");//
+		// 방법 1. List->map->json
+		boolean start = true;
+		for (int i = 0; i < list.size(); i++) {
+			Map map = list.get(i);
+			//System.out.println(map);
+			Iterator<String> keys = map.keySet().iterator();
+			String key = keys.next();
+			String value = (String) map.get(key);
+			if (!start) {
+				result.append(",");
+			} else {
+				start = false;
+			}
+			result.append("\"").append(key).append("\":")//
+					.append("\"").append(value).append("\"");//
+		}
+		result.append("}");//
 		return result.toString();
 	}
 
-	//
+	// object -> json
 	public String toObjectJson(Object vo) throws Exception {// refliction 활용 class or name 값 가져오기
 		StringBuilder result = new StringBuilder();
 		result.append("{");
 		// refliction활용하여 변수접근
 		Field[] fields = vo.getClass().getDeclaredFields();
 		// loop
+		boolean start = true;
 		for (Field field : fields) {
+			System.out.println(field);
 			// field이름만 가져옴
 			String fieldName = field.getName();
-			// 1번째 글자 대문자로
+			// 1번째 글자 잘라서 대문자로 변경 + 1번째 글자 이후의 글자를 문자열 덧셈처리
 			String method = "get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+			// 메소드를 리턴해주는 의미
 			Method m = vo.getClass().getDeclaredMethod(method, null);
 			// 메서드 실행
 			String value = (String) m.invoke(vo, null);
-			System.out.println(fieldName + ":" + value);
-			// to do
+			// System.out.println(fieldName + ":" + value);
+			// ,추가용
+			if (!start) {
+				result.append(",");
+			} else {
+				start = false;
+			}
+			// 내용
+			result.append("\"").append(fieldName).append("\":")// {"key":
+					.append("\"").append(value).append("\"");// {"key":"value"
 		}
 		result.append("}");
 		return result.toString();
 	}
 
-	//
+	// list<object> -> json
 	public String toObjectJson(List<Object> vo) {
 		String result = "";
 		// to do
